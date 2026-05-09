0c5a24a5dab4b5dd14764adab5b1dce6f2c1bd26
rucha287 <maria.sanchez@ucv.ve>
2026-05-09 14:30:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_google-genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
 # Esta es la forma más estable de importar la cadena:
 from langchain.chains import RetrievalQA

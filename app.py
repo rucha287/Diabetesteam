01257925dd66b4f34568f5d7c657ed0344c712e0
rucha287 <maria.sanchez@ucv.ve>
2026-05-09 20:23:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def configurar_asistente():
     
     # 3. Configurar Embeddings (El que te funcionó)
     embeddings = GoogleGenerativeAIEmbeddings(
-        model="gemini-embedding-001",
+        model="models/gemini-embedding-001",
         google_api_key=st.secrets["GEMINI_API_KEY"],
         client_options={"api_version": "v1"}
     )
